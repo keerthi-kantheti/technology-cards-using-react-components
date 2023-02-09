dd84cdbbccf21da483bd8f1f895b1ea41fac44d6
keerthi kantheti <kvkeerthi2001@gmail.com>
2023-02-09 23:07:25 +0000

    technology cards page using React components and also responsive

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import './App.css'
+
+import CardItem from './components/CardItem'
+
+const cardsList = [
+  {
+    id: 1,
+    title: 'Data Scientist',
+    description:
+      'Data scientists gather and analyze large sets of structured and unstructured data',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/data-scientist-img.png',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    title: 'IOT Developer',
+    description:
+      'IoT Developers are professionals who can develop, manage, and monitor IoT devices.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/iot-developer-img.png',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    title: 'VR Developer',
+    description:
+      'A VR developer creates completely new digital environments that people can see.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/vr-developer-img.png',
+    className: 'card-3',
+  },
+  {
+    id: 4,
+    title: 'ML Engineer',
+    description:
+      'Machine learning engineers feed data into models defined by data scientists.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/ml-engineer-img.png',
+    className: 'card-4',
+  },
+]
+
+const App = () => (
+  <div className="cards-bg-container">
+    <h1 className="cards-bg-container-heading">4.0 Technologies</h1>
+    <p className="cards-container-description">
+      Get trained by alumni of IITs and top companies like Amazon,
+      Microsoft,Intel,Nvidia,Quallcom etc. Learn directly from professionals
+      involved in product development
+    </p>
+    <div className="cards-container">
+      <ul>
+        {cardsList.map(eachItem => (
+          <CardItem key={eachItem.id} cardDetails={eachItem} />
+        ))}
+      </ul>
+    </div>
+  </div>
+)
+
+export default App
